823a51c66ff9e4b8ffd140b9be7a5db86ca7e213
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-11-15 17:20:26 +0900

    Created hello world Lamdbda

--- lib/noedjs-14-typescript-cdk-stack.ts
+++ lib/noedjs-14-typescript-cdk-stack.ts
@@ -1,18 +1,15 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class Noedjs14TypescriptCdkStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'Noedjs14TypescriptCdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'Noedjs14TypescriptCdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
+		// defines an AWS Lambda resource
+		const hello = new lambda.Function(this, 'HelloHandler', {
+			runtime: lambda.Runtime.NODEJS_10_X, // execution environment
+			code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" directory
+			handler: 'hello.handler', // file is "hello", function is "handler"
+		});
+	}
 }
